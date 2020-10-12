a36cc82a993de6bd671affdee7ae0a58fb1a1d7b
SmokedKoala <k.azarov@inbox.ru>
2020-10-12 21:50:30 +0300

    continue

--- 2nd_sem/external_data_structures.cpp
+++ 2nd_sem/external_data_structures.cpp
@@ -1,13 +1,89 @@
+#include <bits/stdc++.h>
 #include <iostream>
+#include <Windows.h>
 #include <fstream>
+#include <string>
 
-int main()
+using namespace std;
+
+struct Node {
+	int num;
+	string name;
+	int price;
+};
+
+void convert_to_binary()
 {
-   std::ifstream in("in.txt");
-   std::ofstream out("out.bin", std::ios::binary);
-
-   double d;
-   while(in >> d) {
-      out.write((char*)&d, sizeof d);
-   }
+	string line;
+	ifstream in("in.txt");
+	if (in.is_open())
+    {
+        while (getline(in, line))
+        {
+            cout << line << endl;
+            for( char c : line )
+    		{
+        		bitset<8> bs(c);
+        		cout << bs;
+    		}
+   		 cout << endl;
+        }
+    }
+    in.close(); 
+//   	string s= "Welcome to black list, bitch";
+//    
+//    for( char c : s )
+//    {
+//        bitset<8> bs(c);
+//        cout << bs;
+//    }
+//    cout << endl;
 }
+
+void show_binary()
+{
+	
+}
+
+
+
+int main(){
+	setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "Практическая работа №6 Внешние структуры данных - файлы и алгоритмы их обработки. Азаров Константин, ИКБО-02-19"<< endl;
+    int typeNum;
+    int commandNum=0, newNodeData;
+    string menuChoice="\n1)Преобразовать текстовые данные в двоичный файл\n2)Отобразить все записи двоичного файла\n3)сформировать текстовый файл из записей не по ключевому элементу\n4)Определить неключевой параметр записи по ключу\n5)Выход\n";
+	while(true){
+    	commandNum=0;
+    	while(commandNum !=6){
+    		cout<<menuChoice;
+			cin>>commandNum;
+			switch(commandNum){
+			case(1):
+				cout<<"Выбрано преобразование текстового файла в двоичный\n";
+				convert_to_binary();
+				break;
+			case(2):
+				 cout << "Выбрано отображение записей двоичного файла\n"; 
+				 show_binary();
+				break;
+			case(3):
+				cout << "Выбрано формирование текстового файла из записей не по ключу\n"; 
+				break;
+			case(4):
+				cout << "Выбран вывод неключевого элемента по ключу\n";
+				break;
+			case(5):
+				cout<<"Завершение работы\n";
+				return 0;	
+				break;
+			default:
+				cout<<"Некорректные данные";
+				break;
+			}
+		}
+	}
+}
+
--- 2nd_sem/stack.cpp
+++ 2nd_sem/stack.cpp
@@ -1,71 +1,94 @@
 // выражение записывается посимвольно в стек, в нём считается баланс скобок "(" и "["
 #include <iostream>
-using namespace std;
-
-template <typename TElement>
-struct TNode {
-    TElement data;
-    TNode <TElement> *next;
-
-    TNode (TElement newData, TNode <TElement> *nextNode) {
-        data = newData;
-        next = nextNode;
-    }
-};
-
-template <typename TElement>
-class stackInList {
-private:
-    unsigned int sizeOfStack;
-    TNode <TElement> *currentTop;
-
-public:
-    stackInList() {
-        sizeOfStack = 0;
-        currentTop = NULL;
-    }
-
-    ~stackInList() {
-        while (size())
-            pop();
-    }
-
-    void push (const TElement element) {
-        TNode <TElement> *node = new TNode <TElement> (element, currentTop);
-        sizeOfStack++;
-        currentTop = node;
-    }
-
-    void pop() {
-        sizeOfStack--;
-        TNode <TElement> *node    = currentTop;
-        currentTop = currentTop->next;
-        delete node;
-    }
-
-    TElement top () {
-        return currentTop->data;
-    }
-
-    unsigned int size() {
-        return sizeOfStack;
-    }
-};
+#include <Windows.h>
+using namespace std; 
+  
+/* Link list node */
+struct Node { 
+    char data; 
+    struct Node* next; 
+}; 
+  
+/* Function to remove the last node   
+   of the linked list */
+Node* pop(struct Node* head) { 
+    if (head == NULL) 
+        return NULL; 
+  
+    if (head->next == NULL) { 
+        delete head; 
+        return NULL; 
+    } 
+}
+    
+// Function to push node at head 
+void push(struct Node** head_ref, char new_data){ 
+    struct Node* new_node = new Node; 
+    new_node->data = new_data; 
+    new_node->next = (*head_ref); 
+    (*head_ref) = new_node; 
+} 
 
 
 int main(){
-	stackInList <char>stack;
-	char symb;
-	while (cin>>symb){
-		stack.push(symb);
-		if (isspace(cin.peek()))
+	setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+	int balance_counter1=0,balance_counter2=0 ;
+	Node* head = NULL;
+	cout<<"Практическая работа №4. Стек. Азаров Константин, ИКБО-02-19"<<endl;
+	string ch, choice;
+	while (true){
+		cout<<"Введите строку для анализа: ";
+		while (cin>>ch)
 		{
-			if (cin.get()=='\n')
-				break;	
-			}
-			else
-				break;
+			for (int i =0; i< ch.size();i++)
+				push(&head, ch[i]);
+			if (isspace(cin.peek()))
+			{
+				if (cin.get()=='\n')
+					break;
+			}			
+		} 
+    	cout<<"Найденные скобки\n";
+    	for (Node* temp = head; temp != NULL; temp = temp->next) {
+    		if(temp->data=='('){
+    			balance_counter1++;
+	    		cout<<temp->data;
+			}	
+    		if(temp->data==')'){
+    			balance_counter1--;
+	    		cout<<temp->data;
+			}	
+    		if(temp->data=='['){
+    			balance_counter2++;
+	    		cout<<temp->data;
+			}	
+    		if(temp->data==']'){
+    			balance_counter2--;
+	    		cout<<temp->data;
+			}	
+    		head = pop(head);	
+		}
+    	cout<<"\nБаланс круглых скобок: ";
+		if(balance_counter1==0)
+			cout<<"равное количество";
+		if(balance_counter1>0)
+			cout<<"открывающих больше на "<<balance_counter1;
+		if(balance_counter1<0)
+			cout<<"закрывающих больше на "<<-1*balance_counter1;
+		cout<<endl;	    
+		cout<<"Баланс квадратных скобок: ";
+		if(balance_counter2==0)
+			cout<<"равное количество";
+		if(balance_counter2>0)
+			cout<<"открывающих больше на "<<balance_counter2;
+		if(balance_counter2<0)
+			cout<<"закрывающих больше на "<<-1*balance_counter2;
+		cout<<"\nПовторить ввод?(да/нет): ";
+		cin>>choice;
+		if(choice=="нет")
+			return 0;
+		
 	}
-	cout<<stack.size();
-	return 0;
 }
