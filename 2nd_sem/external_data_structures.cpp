319a2733f458536c7b8cd8d141d3441a5282d8fc
SmokedKoala <k.azarov@inbox.ru>
2020-10-17 12:58:25 +0300

    work_with_files complete

--- 2nd_sem/external_data_structures.cpp
+++ 2nd_sem/external_data_structures.cpp
@@ -6,59 +6,86 @@
 
 using namespace std;
 
+int size;
+
 struct Node {
 	int num;
-	string name;
+	char name[32];
 	int price;
 };
 
-void convert_to_binary()
-{
-	string nameData="", line="";
-	char snum[5];
-	int numData=0, priceData=0;
-	ofstream in("binary_file.bin", ios::binary); 
-	cin>> numData;
-    while (numData!=-1)
-    {
-    	cin>>nameData>>priceData;
-        line+=string(itoa(numData, snum,6))+" "+nameData+" "+string(itoa(priceData,snum,10))+"\n";
-        cin>> numData;
-    }
-    in.write((char*)&line, sizeof(line));   
-    in.close();
 
+void convert_to_binary(){
+	cout<<"Введите количество элементов для записи ";
+	cin>>size;
+	Node node_container[size];
+	for(int i =0;i<size;i++){
+		cin>>node_container[i].num>>node_container[i].name>>node_container[i].price;
+	}
+	ofstream input("binary_file.txt", ios::binary);
+	input.write((char*)node_container, sizeof(node_container));
+	input.close();
 }
 
-void show_binary()
-{
-	string line;
-	ifstream out("binary_file.bin", ios::binary);
-	out.read((char*)&line, sizeof(line));
-	cout<<line;
-	out.close();
+
+void show_binary(){
+	Node node_reader[size];
+	ifstream output("binary_file.txt", ios::binary);
+	output.read((char*)node_reader, sizeof(node_reader));
+	output.close();
+	
+	for (int i =0; i<size; i++){
+		cout<<node_reader[i].num<<" "<<node_reader[i].name<<" "<<node_reader[i].price<<endl;
+	}
 	
 }
 
 void create_txt(){
-	cout<<"Введите названия продуктов\nПри завершении ввода напишите 'Конец'\n";
-	string nameData, line;
-	ifstream out("binary_file.bin", ios::binary); 
-	out.read((char*)&line, sizeof(line));
-	out.close();
-	cin>> nameData;
-	while (nameData!="Конец"){
-	int pos = line.find(nameData);
-	if (pos == -1)
-        cout << "данная запись не найдена: "<<nameData << endl;
-    else
-        cout << "ch " << line[pos]  << endl;	
-    cin>> nameData;
+	string line;
+	char name[32];
+	cout<< "Введите названия продуктов, при завершении ввода введите 'Конец'"<<endl;
+	cin>>name;
+	Node node_reader[size];
+	ifstream output("binary_file.txt", ios::binary);
+	output.read((char*)node_reader, sizeof(node_reader));
+	output.close();
+	while(strcmp(name,"Конец" )!=0){
+		for (int i =0; i<size; i++){
+			if (strcmp(name, node_reader[i].name)==0  )
+				line+=to_string(node_reader[i].num)+" "+node_reader[i].name+" "+to_string(node_reader[i].price)+"\n";
+		}
+		cin>>name;
 	}
-	ofstream in("txt_file.txt", ios::binary);
+	ofstream out("txt_file.txt", std::ios::app);
+	if (out.is_open()){
+		out << line;
+	}
+	out.close();
+	cout<<"Записанные в файл данные: "<<endl;
+	ifstream in ("txt_file.txt");
+	if (in.is_open())
+    {
+        while (getline(in, line))
+        {
+            cout << line << endl;
+        }
+    }
+    in.close();
+}
+
+void find_node(int key){
+	Node node_reader[size];
+	ifstream output("binary_file.txt", ios::binary);
+	output.read((char*)node_reader, sizeof(node_reader));
+	output.close();
 	
-	in.close();
-	
+	for (int i =0; i<size; i++){
+		if (key==node_reader[i].num){
+			cout<<endl<<node_reader[i].num<<" "<<node_reader[i].name<<" "<<node_reader[i].price<<endl;
+			return;
+		}	
+	}
+	cout<<"Не найдено товара с таким номером"<<endl;
 }
 
 
@@ -68,7 +95,6 @@ int main(){
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     cout << "Практическая работа №6 Внешние структуры данных - файлы и алгоритмы их обработки. Азаров Константин, ИКБО-02-19"<< endl;
-    int typeNum;
     int commandNum=0, newNodeData;
     string menuChoice="\n1)Преобразовать текстовые данные в двоичный файл\n2)Отобразить все записи двоичного файла\n3)сформировать текстовый файл из записей не по ключевому элементу\n4)Определить неключевой параметр записи по ключу\n5)Выход\n";
 	while(true){
@@ -78,7 +104,7 @@ int main(){
 			cin>>commandNum;
 			switch(commandNum){
 			case(1):
-				cout<<"Выбрано преобразование текстовых данны в двоичный файл\nПри окончании ввода структур напишите -1\n";
+				cout<<"Выбрано преобразование текстовых данны в двоичный файл\n";
 				convert_to_binary();
 				break;
 			case(2):
@@ -90,7 +116,9 @@ int main(){
 				create_txt();
 				break;
 			case(4):
-				cout << "Выбран вывод неключевого элемента по ключу\n";
+				cout << "Выбран вывод неключевого элемента по ключу\nВведите ключ: ";
+				cin>>newNodeData;
+				find_node(newNodeData);
 				break;
 			case(5):
 				cout<<"Завершение работы\n";
