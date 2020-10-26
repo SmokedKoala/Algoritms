fb7811bb017620ff73f7f0d8a795bca430ed1306
SmokedKoala <k.azarov@inbox.ru>
2020-10-26 10:28:03 +0300

    Auto stash before merge of "master" and "origin/master"

--- 2nd_sem/external_data_structures.cpp
+++ 2nd_sem/external_data_structures.cpp
@@ -15,6 +15,7 @@ struct Node {
 };
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
 void convert_to_binary()
 {
@@ -35,6 +36,9 @@ void convert_to_binary()
     in.close();
 >>>>>>> Stashed changes
 
+=======
+//функция создания бинарного файла и занесения данных в него
+>>>>>>> Stashed changes
 void convert_to_binary(){
 	cout<<"Введите количество элементов для записи ";
 	cin>>size;
@@ -47,7 +51,7 @@ void convert_to_binary(){
 	input.close();
 }
 
-
+//функция отображения данных из бинарного файла
 void show_binary(){
 	Node node_reader[size];
 	ifstream output("binary_file.txt", ios::binary);
@@ -59,7 +63,7 @@ void show_binary(){
 	}
 	
 }
-
+//функция создания текстового файла и занесение в него выбранных структур
 void create_txt(){
 <<<<<<< Updated upstream
 	string line;
@@ -93,7 +97,7 @@ void create_txt(){
     }
     in.close();
 }
-
+//функция поиска структуры в бинарном файле
 void find_node(int key){
 	Node node_reader[size];
 	ifstream output("binary_file.txt", ios::binary);
@@ -146,37 +150,35 @@ int main(){
     cout << "Практическая работа №6 Внешние структуры данных - файлы и алгоритмы их обработки. Азаров Константин, ИКБО-02-19"<< endl;
     int commandNum=0, newNodeData;
     string menuChoice="\n1)Преобразовать текстовые данные в двоичный файл\n2)Отобразить все записи двоичного файла\n3)сформировать текстовый файл из записей не по ключевому элементу\n4)Определить неключевой параметр записи по ключу\n5)Выход\n";
-	while(true){
-    	commandNum=0;
-    	while(commandNum !=6){
-    		cout<<menuChoice;
-			cin>>commandNum;
-			switch(commandNum){
-			case(1):
-				cout<<"Выбрано преобразование текстовых данны в двоичный файл\n";
-				convert_to_binary();
-				break;
-			case(2):
-				cout << "Выбрано отображение записей двоичного файла\n"; 
-				show_binary();
-				break;
-			case(3):
-				cout << "Выбрано формирование текстового файла из записей не по ключу\n"; 
-				create_txt();
-				break;
-			case(4):
-				cout << "Выбран вывод неключевого элемента по ключу\nВведите ключ: ";
-				cin>>newNodeData;
-				find_node(newNodeData);
-				break;
-			case(5):
-				cout<<"Завершение работы\n";
-				return 0;	
-				break;
-			default:
-				cout<<"Некорректные данные";
-				break;
-			}
+    commandNum=0;
+    while(true){
+    	cout<<menuChoice;
+		cin>>commandNum;
+		switch(commandNum){
+		case(1):
+			cout<<"Выбрано преобразование текстовых данны в двоичный файл\n";
+			convert_to_binary();
+			break;
+		case(2):
+			cout << "Выбрано отображение записей двоичного файла\n"; 
+			show_binary();
+			break;
+		case(3):
+			cout << "Выбрано формирование текстового файла из записей не по ключу\n"; 
+			create_txt();
+			break;
+		case(4):
+			cout << "Выбран вывод неключевого элемента по ключу\nВведите ключ: ";
+			cin>>newNodeData;
+			find_node(newNodeData);
+			break;
+		case(5):
+			cout<<"Завершение работы\n";
+			return 0;	
+			break;
+		default:
+			cout<<"Некорректные данные";
+			break;
 		}
 	}
 }
