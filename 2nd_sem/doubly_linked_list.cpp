e38580b7660d4dbcdae83694a06ab08d4399984e
SmokedKoala <k.azarov@inbox.ru>
2020-11-06 13:28:21 +0300

    complete

--- 2nd_sem/doubly_linked_list.cpp
+++ 2nd_sem/doubly_linked_list.cpp
@@ -1,107 +1,240 @@
-// C++ program to delete a node from
-// Doubly Linked List
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <windows.h>
 using namespace std;
 
-/* a node of the doubly linked list */
-class Node 
-{ 
-	public:
-	int data; 
-	Node* next; 
-	Node* prev; 
-}; 
+struct Node {                //структура звена списка 
+	int value;          //передаваемое значение
+	Node* next, * prev;      //указатели на следующий и предыдущий элементы
+};
 
-/* Function to delete a node in a Doubly Linked List. 
-head_ref --> pointer to head node pointer. 
-del --> pointer to node to be deleted. */
-void deleteNode(Node** head_ref, Node* del) 
-{ 
-	/* base case */
-	if (*head_ref == NULL || del == NULL) 
-		return; 
+struct LDS {
+	Node* head = nullptr;     //инициализация начала и конца списка, размера ЛДС 
+	Node* tail = nullptr;
+	int count = 0;
 
-	/* If node to be deleted is head node */
-	if (*head_ref == del) 
-		*head_ref = del->next; 
+	int push()
+	{
+		Node* search = new Node;
+		cout << "Введите элемент, после которого необходимо добавить новый:\t";
+		cin >> search->value;
 
-	/* Change next only if node to be 
-	deleted is NOT the last node */
-	if (del->next != NULL) 
-		del->next->prev = del->prev; 
+		Node* el = new Node;
+		cout << "Введите новый элемент:\t";
+		cin >> el->value;
 
-	/* Change prev only if node to be 
-	deleted is NOT the first node */
-	if (del->prev != NULL) 
-		del->prev->next = del->next; 
+		Node* fl = head;
+		for (int i = 0; i < count; i++)
+		{
+			if (fl->value == search->value)
+			{
+				Node* second = fl->next;
+				fl->next = el;
+				el->next = second;
+				count++;
+				cout << "Элемент успешно добавлен\n";
+				return i;
+			}
+			else
+				fl = fl->next;
+		}
 
-	/* Finally, free the memory occupied by del*/
-	free(del); 
-	return; 
-} 
+		cout << "Элемент с номером " << search->value << " не найден\n";
+		return -1;
+	}
 
-/* UTILITY FUNCTIONS */
-/* Function to insert a node at the
-beginning of the Doubly Linked List */
-void push(Node** head_ref, int new_data) 
-{ 
-	/* allocate node */
-	Node* new_node = new Node();
+	void push_back(bool flag) {
 
-	/* put in the data */
-	new_node->data = new_data; 
+		Node* element = new Node; //выделение памяти под новый элемент структуры
+		if (flag) cout << "Введите элемент, который необходимо добавить в конец ЛДС:\t";
+		cin >> element->value;  //добавляем значение в структуру
+		count++;
+		if (!head) {          //если список пуск
+			head = element;       //т.к элемент единственный, 
+			tail = head;      //он является и хвостом и головой
+		}
+		else {
+			element->prev = tail; //предыдущий элемент списка относительно добавленного, будет последним(хвостом)
+			tail->next = element; //следующий элемент за хвостом это добавляемый элемент списка
+			tail = element;       //присваивание элементу статуса хвоста
+		}
+		if (flag) cout << "Элемент успешно добавлен в конец ЛДС" << endl;
+	}
 
-	/* since we are adding at the beginning, 
-	prev is always NULL */
-	new_node->prev = NULL; 
+	void push_front() {
 
-	/* link the old list off the new node */
-	new_node->next = (*head_ref); 
+		Node* element = new Node;
+		cout << "Введите элемент, который необходимо добавить в начало ЛДС:\t";
+		cin >> element->value;
+		count++;
+		if (!head)
+		{
+			head = element;
+			tail = head;
+		}
+		else
+		{
+			element->next = head;      //следующий элемент за добавляемым элементом является хвост
+			head->prev = element;      //перед головой находится добавляемый элемент
+			head = element;           //присваивание элементу статуса головы
+		}
+		cout << "Элемент успешно добавлен в начало ЛДС" << endl;
+	}
 
-	/* change prev of head node to new node */
-	if ((*head_ref) != NULL) 
-		(*head_ref)->prev = new_node; 
+	void pop_back() {
+		if (count != 0) {               //если ЛДС не пустой
+			cout << tail->value << endl;
+			if (count > 1) {         //если в ЛДС находится больше одного элемента
+				Node* element = tail;     //указываем на то, что будет использоваться хвост
+				tail = tail->prev; //присваиваем статус хвоста предыдущему элементу
+				tail->next = nullptr;     //указываем на то, что за элементом пусто
+				delete element;     //удаляем бывший хвост
+				count--;        //уменьшаем размер ЛДС
+			}
+			else {               //если в ЛДС находится всего один элемент
+				head = tail = 0;        //присваиваем ему значение ноль
+				count--;        //уменьшаем размер ЛДС
+			}
+			cout << "Последний элемент ЛДС удалён\n";
+		}
+		else cout << "ЛДС пуст, удалять нечего" << endl;
+	}
 
-	/* move the head to point to the new node */
-	(*head_ref) = new_node; 
-} 
+	void pop_front() {
+		if (count != 0) {
+			cout << head->value << endl;
+			if (head->next) {          //если в ЛДС больше одного элемента 
+				Node* element = head; //указываем на то, что будем использовать голову
+				head = head->next;	//присваиваем статус головы следующему за бывшей головой элементу 
+				head->prev = nullptr;  //указываем на то, что перед головой пусто
+				delete element;     //удаляем бывшую голову
+				count--;			//уменьшаем размер ЛДС
+			}
+			else if (head == tail) {    //если элемент один в ЛДС
+				head->next = nullptr;  //указываем, что за головой пусто
+				head = nullptr;       //указываем на то, что голова тоже пуста
+				delete head;        //удаляем единственный элемент
+				count = 0;        //присваиваем ноль размеру ЛДС
+			}
+			cout << "Первый элемент ЛДС удалён\n";
+		}
+		else cout << "ЛДС пуст, удалять нечего" << endl;
+	}
 
-/* Function to print nodes in a given doubly linked list 
-This function is same as printList() of singly linked list */
-void printList(Node* node) 
-{ 
-	while (node != NULL) 
-	{ 
-		cout << node->data << " "; 
-		node = node->next; 
-	} 
-} 
+	void print() {
+		Node* first = this->head;
+		for (int i = 0; i < count; i++)
+		{
+			cout << first->value << ' ';
+			first = first->next;
+		}
+		cout << endl;
+	}
 
-/* Driver code*/
-int main() 
-{ 
-	/* Start with the empty list */
-	Node* head = NULL; 
+	void print_back() {
+		Node* first = this->tail;
+		for (int i = count; i > 0; --i)
+		{
+			cout << first->value << ' ';
+			first = first->prev;
+		}
+		cout << endl;
+	}
 
-	/* Let us create the doubly linked list 10<->8<->4<->2 */
-	push(&head, 2); 
-	push(&head, 4); 
-	push(&head, 8); 
-	push(&head, 10); 
+	~LDS() {
+		Node* first;
+		for (int i = 0; i < count; i++)
+		{
+			first = head;
+			head = head->next;
+			delete first;
+		}
 
-	cout << "Original Linked list "; 
-	printList(head); 
+		cout << "Удаление прошло успешно\n";
+	}
+};
 
-	/* delete nodes from the doubly linked list */
-	deleteNode(&head, head); /*delete first node*/
-	deleteNode(&head, head->next); /*delete middle node*/
-	deleteNode(&head, head->next); /*delete last node*/
+void menu(LDS* l)
+{
+	cout << "Введите 10 элементов для добавления в конец:\n";
+	for (int i = 0; i < 10; i++)
+		l->push_back(false);
+	cout << "\n";
 
-	/* Modified linked list will be NULL<-8->NULL */
-	cout << "\nModified Linked list "; 
-	printList(head); 
+	int n = 0;
+	cout << "Список команд:\n";
+	cout << "1) Добавление элемента "<< "2) Удаление элемента "<< "3) Вывод ЛДС на экран "<< "0) Выход из программы\n\n";
 
+	while (true)
+	{
+		Node* search = new Node;
+		cout << "Новая команда:\t";
+		cin >> n;
+		string value;
+		switch (n)
+		{
+		case 0:
+			exit(0);
+		case 1:
+			cout << "\t1) В начало ЛДС\n";
+			cout << "\t2) В конец ЛДС\n";
+			cout << "\t3) После заданного элемента\n";
+			cout << "Ваш выбор: ";
+			cin >> n;
+			switch (n)
+			{
+			case 1:
+				l->push_front();
+				break;
+			case 2:
+				l->push_back(true);
+				break;
+			case 3:
+				l->push();
+				break;
+			}
+			break;
+		case 2:
+			cout << "\t1) Первый элемент\n";
+			cout << "\t2) Последний элемент\n";
+			cout << "Ваш выбор: ";
+			cin >> n;
+			switch (n)
+			{
+			case 1:
+				l->pop_front();
+				break;
+			case 2:
+				l->pop_back();
+				break;
+			}
+			break;
+		case 3:
+			cout << "\t1) По порядку\n";
+			cout << "\t2) В обрятном порядке\n";
+			cout << "Ваш выбор: ";
+			cin >> n;
+			switch (n)
+			{
+			case 1:
+				l->print();
+				break;
+			case 2:
+				l->print_back();
+				break;
+			}
+			break;
+		}
+		cout << "\n";
+
+	}
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	LDS* l = new LDS;
+	menu(l);
 	return 0;
-} 
-
-
+}
